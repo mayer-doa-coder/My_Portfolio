a2d2fdeaf1ef7c7535d61b012614b480546beb07
mayer-doa-coder <hasan2107004@stud.kuet.ac.bd>
2026-01-06 18:44:27 +0600

    modify: remove gradient bg from card in sections

--- myPortfolio/astro.config.mjs
+++ myPortfolio/astro.config.mjs
@@ -7,6 +7,9 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
   vite: {
     plugins: [tailwindcss()]
   },
